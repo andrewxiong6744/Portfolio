86543c947839ff3f7c104fcf1a75b2e7f50d852f
andrewxiong6744 <143228894+andrewxiong6744@users.noreply.github.com>
2025-11-02 17:45:40 -0600

    Update PortfolioContent.tsx

--- src/components/PortfolioContent.tsx
+++ src/components/PortfolioContent.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import svgPaths from "../imports/svg-sofui92bd4";
-import imgGeneratedImageNovember012025824Pm1 from "../src/assets/Frame 1.png";
+import bgImage from "@/assets/frame1.png";
+
 
 // Interactive outline component with hover effects
 interface InteractiveOutlineProps {
